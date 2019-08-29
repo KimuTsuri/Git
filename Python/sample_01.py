c1dc853ff2d77e231a5ebfdc859b7893d219ed24
KimuTsuri <ktsuri@hpx.phys.ocha.ac.jp>
2019-08-29 20:46:12 +0900

    add new file

---
+++ Python/sample_01.py
@@ -0,0 +1,13 @@
+names = ['田中', '佐藤', '吉田']
+
+if '田中' in names:
+  print('田中さんがいました')
+else:
+  print('田中さんはいません')
+
+if '滝沢' in names:
+  print('滝沢さんがいました')
+else:
+  print('滝沢さんはいません')
+
+print('処理を終了します')
