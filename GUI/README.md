e9e8ca94eb3e4d121ba209a286f5d8b07cf4f540
KimuTsuri <kimu-tsuri@Kimu-MacBook.local>
2019-12-17 15:50:02 +0900

    add new file

---
+++ GUI/README.md
@@ -0,0 +1,2 @@
+# GUI
+GUI (Graphical User Interface) study
---
+++ GUI/Tkinter/first.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('はじめてのtkinter')
+input_box = tk.Entry(root)
+input_box.pack()
+root.mainloop()
